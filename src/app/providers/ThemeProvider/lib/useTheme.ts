6395da7edc46ea29732a3ba25257846995ee5874
Your Name <you@example.com>
2023-12-26 08:03:35 +0300

    add new theme and hot react reload

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,7 +10,25 @@ export function useTheme(): UseThemeResult {
   const { setTheme, theme } = useContext(ThemeContext)
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+    let newTheme: Theme
+
+    switch (theme) {
+      case Theme.DARK: {
+        newTheme = Theme.LIGHT
+        break
+      }
+      case Theme.LIGHT: {
+        newTheme = Theme.ORANGE
+        break
+      }
+      case Theme.ORANGE: {
+        newTheme = Theme.DARK
+        break
+      }
+      default: {
+        newTheme = Theme.LIGHT
+      }
+    }
 
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
     document.body.className = newTheme
