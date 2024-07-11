8bb2a03ca1cb1f76d2da3b6e488dea25064f1984
Your Name <you@example.com>
2024-07-11 20:19:10 +0300

    parallel

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { render } from 'react-dom'
 import { App } from 'app/App'
+import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 import 'app/styles/index.scss'
