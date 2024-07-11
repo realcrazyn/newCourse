7222952f8bf0ac97b736a8370af7e1342a443af8
Your Name <you@example.com>
2024-07-11 20:18:41 +0300

    adnd enother

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from 'react-dom'
-import { App } from 'app/App'
 import { BrowserRouter } from 'react-router-dom'
+import { App } from 'app/App'
 import { StoreProvider } from 'app/providers/StoreProvider'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 import 'app/styles/index.scss'
