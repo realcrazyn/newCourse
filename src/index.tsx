eb879cf6e81e196d762cd1ac12c18a927d7853b9
Your Name <you@example.com>
2024-07-12 09:16:22 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-
 import { App } from 'app/App'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { App } from 'app/App'
 import { StoreProvider } from 'app/providers/StoreProvider'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 import 'app/styles/index.scss'
 import 'shared/config/i18n/i18n'
-import { StoreProvider } from 'app/providers/StoreProvider'
 import { ErrorBoundary } from 'app/providers/ErrorBoundary'
 
 render(
